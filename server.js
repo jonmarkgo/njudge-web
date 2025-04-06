4cb3d47ba10efb6b43cbc4375fcecfc2636a6e82
Jonathan Gottfried <jonmarkgo@gmail.com>
2025-04-06 12:28:20 -0400

    expand route listing

--- server.js
+++ server.js
@@ -2561,11 +2561,35 @@ app.listen(port, () => {
         console.log(`Found dip.master at '${dipMasterPath}'. Performing initial sync...`);
         syncDipMaster().catch(err => console.error("[Startup Sync Error]", err));
     }
-    app._router.stack.forEach(function(r){
-        if (r.route && r.route.path){
-          console.log(r.route.path)
+    function print (path, layer) {
+        if (layer.route) {
+          layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))))
+        } else if (layer.name === 'router' && layer.handle.stack) {
+          layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))))
+        } else if (layer.method) {
+          console.log('%s /%s',
+            layer.method.toUpperCase(),
+            path.concat(split(layer.regexp)).filter(Boolean).join('/'))
         }
-      })
+      }
+      
+      function split (thing) {
+        if (typeof thing === 'string') {
+          return thing.split('/')
+        } else if (thing.fast_slash) {
+          return ''
+        } else {
+          var match = thing.toString()
+            .replace('\\/?', '')
+            .replace('(?=\\/|$)', '$')
+            .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//)
+          return match
+            ? match[1].replace(/\\(.)/g, '$1').split('/')
+            : '<complex:' + thing.toString() + '>'
+        }
+      }
+      
+      app._router.stack.forEach(print.bind(null, []))
 });
 
 process.on('SIGINT', () => {
